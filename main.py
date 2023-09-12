220ab77b7cab83c05664c14d2e8365dea37167e8
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2023-09-12 19:06:30 +0100

    Updated chatting features

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
+import time
+
 import streamlit as st
 from gradio_client import Client
 
 # Headings
 st.markdown("# Welcome to Summarizer-Pro")
 st.write("Hi! I am your Assistant Summarizer! Give me any text and I'm going to summarize it for you!")
-
-# Create a summarizer model
-if "gradio-model" not in st.session_state:
-    st.session_state["gradio-model"] = 'summarizer-101'
 
 # Create chat history
 if "messages" not in st.session_state:
@@ -16,7 +14,7 @@ if "messages" not in st.session_state:
 # Display chat history when app is rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown("content")
+        st.markdown(message["content"])
 
 # Accept user input for chat
 if prompt := st.chat_input("Enter text to be summarized"):
@@ -33,11 +31,13 @@ if prompt := st.chat_input("Enter text to be summarized"):
         message_placeholder = st.empty()
         full_response = " "
         # Get summary from Summarizer
-        client = Client("https://theosphil-meta-llama-llama-2-13b-hf.hf.space/")
+        client = Client("https://theosphil-facebook-bart-large-cnn.hf.space/")
         result = client.predict(
                         prompt,	 # str in 'Input' Textbox component
                         api_name="/predict"
         )
         full_response += result
         message_placeholder.markdown(full_response)
+
+    # Add summarizer reply to chat history
     st.session_state.messages.append({'role': 'assistant', 'content': full_response})
