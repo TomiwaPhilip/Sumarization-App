bc350c68400826045cb958c86d7299423d2dfdd1
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2023-09-12 17:08:45 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.markdown("Welcome to Summarizer-Pro")
+st.markdown("# Welcome to Summarizer-Pro")
+
+st.write("Hi! I am your Assistant Sumarizer! Give me any text and I'm going to summarize it for you!")
+
+st.chat_input("Enter text to be summarized...")
